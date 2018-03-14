07fb53ddcca75faa874146530790eb5dcbcd3cee
Eli Meschures <meschures.eli@gmail.com>
2018-03-14 10:07:40 -0700

    update todo model

--- models/Todo.js
+++ models/Todo.js
@@ -1,1 +1,9 @@
-// todo model...
+const
+    mongoose = require('mongoose'),
+    todoSchema = new mongoose.Schema({
+        body: String,
+        completed: { type: Boolean, default: false }
+    })
+
+const Todo = mongoose.model('Todo', todoSchema)
+module.exports = Todo  
